607765ddd0f7d764bee845f507788d2a196b4020
Lucas Oliveira <olivmath@protonmail.com>
2025-02-02 02:14:20 -0300

    fix: transferFrom mod5 aula7

--- mod5/playground/aula7/transferFrom.js
+++ mod5/playground/aula7/transferFrom.js
@@ -13,9 +13,10 @@ import abi from "./abi.js";
 const ALICE_ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 const BOB_ADDRESS = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
 const EVE_ADDRESS = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC";
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const aliceContractConnection = getContract({
-  address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  address: CONTRACT_ADDRESS,
   abi: abi,
   client: {
     public: createPublicClient({
@@ -33,7 +34,7 @@ const aliceContractConnection = getContract({
 });
 
 const bobContractConnection = getContract({
-  address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  address: CONTRACT_ADDRESS,
   abi: abi,
   client: {
     public: createPublicClient({
